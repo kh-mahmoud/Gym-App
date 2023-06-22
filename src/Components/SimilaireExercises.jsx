f215d8a994d352c505247ee8bce165a3d33b6f25
Khouchane Mahmoud <97807779+Uno486@users.noreply.github.com>
2023-06-22 13:57:49 +0100

    new

--- src/Components/SimilaireExercises.jsx
+++ src/Components/SimilaireExercises.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Typography, Box, Stack } from '@mui/material';
-
-import HorizontalScrollbar from './HorizontalScrollbar';
 import Loader from './Loader';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
   <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
